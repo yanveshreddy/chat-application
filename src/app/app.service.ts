86624a689573d0b65ece77c8ddca066c3e692b3c
yanveshreddy <anvesh.learnings@gmail.com>
2019-10-14 22:44:21 +0530

    user module commit

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { observable } from 'rxjs';
+
+import { Cookie } from 'ng2-cookies/ng2-cookies';
+
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+
+import { HttpErrorResponse,HttpParams} from '@angular/common/http';
+
+import 'rxjs/operator/catch';
+import 'rxjs/operator/do';
+import 'rxjs/operator/toPromise';
+import { Observable } from 'rxjs/observable';
+//import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  private url= 'https://chatapi.edwisor.com';
+
+  constructor(public http: HttpClient) { }
+
+  public signupFunction(data): any{
+
+
+  }
+
+  public signinFunction(data): any{
+
+  }
+  public setUserInfoInLocalStorage=(data) =>{
+
+  }
+  public getUserInfoInLocalStorage=() =>{
+    
+  }
+private handleError(err: HttpErrorResponse) {
+
+    console.log(err.message);
+    return Observable.throw(err.message);
+    
+   }
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrService } from 'ngx-toastr';
+
 
 import {RouterModule,Routes} from '@angular/router';
 
